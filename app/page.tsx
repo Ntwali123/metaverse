17442008375726c113123db240e476a33ca81561
nshutiyvonne1@gmail.com <ntwalidavid90@gmail.com>
2025-01-17 11:03:57 +0200

    added a file

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import Image from "next/image";
-import Navbar from "@/components/Navbar";
+import Hero from "@/sections/Hero";
+import About from "@/sections/About";
+import Explore from "@/sections/Explore";
+import GetStarted from "@/sections/GetStarted";
+import WhatsNew from "@/sections/WhatsNew";
+import World from "@/sections/World";
+import Insights from "@/sections/Insights";
+import Feedback from "@/sections/Feedback";
+// import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
    <>
-   <Navbar />
+   {/*<Navbar />*/}
+       <Hero />
+       <About />
+       <Explore />
+       <GetStarted />
+       <WhatsNew />
+       <World />
+       <Insights />
+       <Feedback />
    </>
   );
 }
